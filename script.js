b245430107f4773d561125f33a248f70a5a6b4bb
NickKumar <Timerbob@gmail.com>
2024-01-26 20:24:39 -0700

    More styling and resizing

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
 const main = document.querySelector("#main");
 const start = document.querySelector(".button");
-const input = document.querySelector(".input");
+// const input = document.querySelector(".input");
 start.addEventListener("click", sort);
 
 let numArr = [];
 
 let vw = innerWidth / 100;
-let vh = innerWidth / 100;
+let vh = innerHeight / 100;
+
+console.log(innerHeight);
+console.log(innerHeight * 0.9);
 
 const generateNumbers = (maxNum) => {
   numArr = [];
   for (let i = 0; i < maxNum; i++) {
-    numArr.push(i);
+    numArr.push(i + 1);
   }
 };
 
@@ -24,36 +27,48 @@ const randomizeArray = (array) => {
 };
 
 const printToScreen = (n) => {
-  const newElement = document.createElement("p");
+  const newElement = document.createElement("div");
+  let width = `${document.querySelector("#main").clientWidth / 9}px`;
+  let height = `${document.querySelector("#main").clientHeight / 10}px`;
+  let style = () => {
+    if (vh > vw) {
+      return "width: 85%";
+    } else {
+      return "height: 85%";
+    }
+  };
+
+  console.log(style());
+
+  let content = `<div class="box-number" style="${style()}"><p>${n}</p></div>`;
+
   newElement.classList.add("box");
-  newElement.textContent = `${n}`;
+  newElement.style.width = width;
+  newElement.style.height = height;
+  newElement.innerHTML = content;
   main.appendChild(newElement);
-  //   console.log("cha");
 };
 
-// printToScreen("p");
-
-// console.log(numArr);
 console.log(start);
 
 const init = () => {
   numArr.map((el) => printToScreen(el));
-  //   console.log("looop");
   dialog.close();
 };
 
 function sort() {
-  if (input.value < 2 || input.value > 1000) {
-    console.log(input.value);
-    alert("Between 2-1000");
-    return;
-  } else {
+  // if (input.value < 2 || input.value > 1000) {
+  //   console.log(input.value);
+  //   alert("Between 2-1000");
+  //   return;
+  // } else
+  {
     main.innerHTML = "";
-    console.log(input.value);
-    console.log("wooo");
+    // console.log(input.value);
+    // console.log("wooo");
 
-    // TODO: change the input value to no longer be selectable on the first iteration. create the size based on the screen size using vh and vw values.
-    generateNumbers(input.value);
+    // generateNumbers(input.value);
+    generateNumbers(64);
     randomizeArray(numArr);
     init();
   }
